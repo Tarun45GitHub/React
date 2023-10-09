19f6835c5a8b094c86151b2af690ab92f39d71c3
Tarun Dalbera <td2545@it.jgec.ac.in>
2023-10-10 04:54:37 +0530

    intervew

--- 03hooksproject/src/App.jsx
+++ 03hooksproject/src/App.jsx
@@ -7,11 +7,22 @@ function App() {
  let [counter,setCounter]=useState(5)
 
   // let counter=5;
+  /*
   const addvalue=()=>{
     if(counter<20) counter=counter+1;
     // console.log("clicked",counter);
     setCounter(counter)
   }
+*/
+const addvalue=()=>{
+  // console.log("clicked",counter);
+  // it increment counter 5 times
+  setCounter(counter=>counter+1)
+  setCounter(counter=>counter+1)
+  setCounter(counter=>counter+1)
+  setCounter(counter=>counter+1)
+  setCounter(counter=>counter+1)
+}
   const removevalue=()=>{
     if(counter>=1) counter=counter-1;
     // console.log("clicked",counter);
