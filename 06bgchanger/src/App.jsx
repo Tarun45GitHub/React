52f7b65fae645445b0bfa980fa94eaceb96b4afa
Tarun Dalbera <td2545@it.jgec.ac.in>
2023-10-18 12:03:29 +0530

    add

--- 06bgchanger/src/App.jsx
+++ 06bgchanger/src/App.jsx
@@ -1,26 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
   const [color, setColor] = useState("black")
-
   return (
-    <>
-     <div className="">
-      <button value={"blue"}>blue</button>
-      <button value={"blue"}>yellow</button>
-      <button value={"blue"}></button>
-      <button value={"blue"}>blue</button>
-      <button value={"blue"}>blue</button>
-      <button value={"blue"}>blue</button>
-      <button value={"blue"}>blue</button>
-      <button value={"blue"}>blue</button>
-      <button value={"blue"}>blue</button>
-     </div>
-      
-    </>
+    <div className='main'
+    style={{backgroundColor: color}}>
+    <div className='container' >
+      test
+    </div>
+    </div>
   )
 }
 
